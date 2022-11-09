123430652ee6a34f26d3e2234e24288e6b1ebfaa
Koen Swaanen <koen.swaanen@greenstack.be>
2022-11-09 09:07:24 +0100

    first commit

---
+++ cqrs-es-exercise-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cqrs_es_exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
